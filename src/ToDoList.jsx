1f35b79faf1b3194a4453de17f3e1ef2fcda804d
Adam <adammiller.2000nj@gmail.com>
2024-02-12 15:01:09 -0500

    added some starting js to buttons and input

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,11 +2,11 @@ import React, { useState} from 'react';
 
 function ToDoList() {
 
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState(["Eat breakfast", "Take shower", "Walk the dog"]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event) {
-
+        setNewTask(event.target.value)
     };
 
     function addTask() {
@@ -25,7 +25,25 @@ function ToDoList() {
 
     };
 
-    return(<></>)
+    return(<>
+        <div className='to-do-list'>
+            <h1>To-Do-List</h1>
+            <div>
+                <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChange} />
+                <button className="add-btn" onClick={addTask}>Add</button>
+            </div>
+            <ol>
+                {tasks.map((task, index) => 
+                    <li key={index}>
+                        <span className="text">{task}</span>
+                        <button className="del-btn" onClick={() => deleteTask(index)}>Delete</button>
+                        <button className="move-btn" onClick={() => moveTaskUp(index)}>Move Up</button>
+                        <button className="move-btn" onClick={() => moveTaskDown(index)}>Move Down</button>
+                    </li>
+                )}
+            </ol>
+        </div>
+    </>)
 };
 
 export default ToDoList;
